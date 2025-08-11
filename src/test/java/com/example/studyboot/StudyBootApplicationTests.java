a1699fdcd613ce9e69eb0a00aae8fc5f656f47ce
Rowoon <dev.rowoon@gmail.com>
2025-08-11 13:55:03 +0900

    init

---
+++ src/test/java/com/example/studyboot/StudyBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
