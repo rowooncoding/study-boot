af6ebf2f9307c58451581953a9c50b40f376991e
Rowoon <dev.rowoon@gmail.com>
2025-08-20 11:26:55 +0900

    오토와이어링 사용하여 주입

--- src/main/java/com/example/studyboot/TestController.java
+++ src/main/java/com/example/studyboot/TestController.java
@@ -1,16 +1,14 @@
 package com.example.studyboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    private final TestService testService;
 
-    // 생성자 주입
-    public TestController(TestService testService) {
-        this.testService = testService;
-    }
+    @Autowired
+    private TestService testService;
 
     @GetMapping("/api/v1/test")
     public String test() {
